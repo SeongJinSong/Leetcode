1367b7be7d79de35abc62e85768df666634fe082
송성진 <seongjin_song@tmax.co.kr>
2022-07-04 08:25:08 +0900

    Initial commit

---
+++ src/main/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,7 @@
+package com.study.Leetcode.medium;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOflongestSubstring(String s){
+        return 1;
+    }
+}
---
+++ src/test/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,11 @@
+package com.study.Leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class LongestSubstringWithoutRepeatingCharactersTest {
+    @Test
+    public lengthOflongestSubstring(){
+    }
+}
