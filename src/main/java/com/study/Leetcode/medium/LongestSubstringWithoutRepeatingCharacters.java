567665cb8c8089d1d927149eac52140ae74729d3
seongjin_song <seongjin_song@tmax.co.kr>
2022-07-04 09:26:49 +0900

    LongestSubstringWithoutRepeatingCharacters

--- src/main/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,37 @@
 package com.study.Leetcode.medium;
 
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharacters {
-    public int lengthOflongestSubstring(String s){
-        return 1;
+    public static void main(String[] args) {
+        LongestSubstringWithoutRepeatingCharacters test = new LongestSubstringWithoutRepeatingCharacters();
+        int abcabcbb = test.lengthOfLongestSubstring("abcabcbb");
+        System.out.println(abcabcbb);
+
+    }
+    public int lengthOfLongestSubstring(String s){
+        int max = 0;
+        if(s.length()==1) return 1;
+        for(int i=0;i<s.length();i++){
+            HashSet hs = new HashSet();
+            hs.add(s.charAt(i));
+            for(int j=i+1;j<s.length();j++){
+                if(hs.contains(s.charAt(j))){
+                    if (hs.size() > max) {
+                        max = hs.size();
+                        break;
+                    }
+                    break;
+                }
+                hs.add(s.charAt(j));
+                if (j == s.length() - 1) {
+                    if (hs.size() > max) {
+                        max = hs.size();
+                    }
+                    break;
+                }
+            }
+        }
+        return max;
     }
 }
--- src/test/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/com/study/Leetcode/medium/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -6,6 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LongestSubstringWithoutRepeatingCharactersTest {
     @Test
-    public lengthOflongestSubstring(){
+    void lengthOfLongestSubstring(){
+        LongestSubstringWithoutRepeatingCharacters obj = new LongestSubstringWithoutRepeatingCharacters();
+        assertEquals(obj.lengthOfLongestSubstring("abcabcbb"), 3);
+        assertEquals(obj.lengthOfLongestSubstring("bbbb"), 1);
+        assertEquals(obj.lengthOfLongestSubstring("pwwkew"), 3);
+        assertEquals(obj.lengthOfLongestSubstring(" "), 1);
+        assertEquals(obj.lengthOfLongestSubstring("au"), 2);
+        assertEquals(obj.lengthOfLongestSubstring("jbpnbwwd"), 4);
     }
 }
