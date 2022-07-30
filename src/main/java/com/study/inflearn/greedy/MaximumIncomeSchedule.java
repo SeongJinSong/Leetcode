baaa444a7e545e493ff9353e51dc35d408ccb9f5
송성진 <seongjin_song@tmax.co.kr>
2022-07-30 13:33:38 +0900

    최대 수입 스케쥴 (Greedy, PQ) - solution

--- src/main/java/com/study/inflearn/greedy/MaximumIncomeSchedule.java
+++ src/main/java/com/study/inflearn/greedy/MaximumIncomeSchedule.java
@@ -14,23 +14,38 @@ public class MaximumIncomeSchedule {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int max = 0;
+        ArrayList<Node> a = new ArrayList<>();
         Map<Integer, ArrayList<Integer>> m = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int val = sc.nextInt();
             int due = sc.nextInt();
-            m.computeIfAbsent(due, k -> new ArrayList<>());
-            m.get(due).add(val);
+            m.computeIfAbsent(due, k -> new ArrayList<>()).add(val);
             max = Math.max(max, due);
+            a.add(new Node(val, due));
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int ans = 0;
-        while (max>0) {
-            //due가 max보다 크거나 같은것중 제일 큰것 선택
-            if(m.get(max)!=null){
-                pq.addAll(m.get(max));
+        int level = max;
+        while (level>0) {
+            //due가 level보다 크거나 같은것중 제일 큰것 선택
+            if(m.get(level)!=null){
+                pq.addAll(m.get(level));
             }
             ans += pq.peek()!=null?pq.poll():0;
-            max--;
+            level--;
+        }
+        System.out.println(ans);
+        System.out.println("----------");
+        ans = 0;
+        pq.clear();
+        a.sort((x, y) -> y.due - x.due);
+        int cur=0;
+        for (int i = max; i >0; i--) {
+            for (; cur < n; cur++) {
+                if(a.get(cur).due<i)break;
+                pq.add(a.get(cur).val);
+            }
+            ans+=pq.peek()!=null?pq.poll():0;
         }
         System.out.println(ans);
     }
