50897f7eaeec192f23f334ec08d0aff36ebc3d71
송성진 <seongjin_song@tmax.co.kr>
2022-08-01 22:02:07 +0900

    모든 아나그램 찾기 - hm.equals를 사용하여 개선

--- src/main/java/com/study/inflearn/hashmap/FindAllAnagrams.java
+++ src/main/java/com/study/inflearn/hashmap/FindAllAnagrams.java
@@ -16,27 +16,20 @@ public class FindAllAnagrams {
         int len = t.length();
         int ans = 0;
         HashMap<Character, Integer> hm = new HashMap<>();
+        HashMap<Character, Integer> hm2 = new HashMap<>();
         for (int i = 0; i < len-1; i++) {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
+        for (int i = 0; i < len; i++) {
+            hm2.put(t.charAt(i), hm2.getOrDefault(t.charAt(i), 0) + 1);
+        }
         for (int i = len - 1; i < s.length(); i++) {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
-            if(isAnagram(hm, t))ans++;
+            if(hm.equals(hm2))ans++;
             int lt = i-len+1;
             hm.put(s.charAt(lt), hm.get(s.charAt(lt)) - 1);
             if(hm.get(s.charAt(lt))==0) hm.remove(s.charAt(lt));
         }
         return ans;
-    }
-
-    private boolean isAnagram(HashMap<Character, Integer> hm, String t) {
-        //자바는 call by value 이므로 참조값을 가리키는 주소값을 복사해서 파라미터로 넘겨준다.
-        //새로운 객체로 new를 하면, 원본 hm을 가리치는 주소값이 새로운 객체를 가리키게 된다.
-        hm = (HashMap<Character, Integer>)hm.clone();
-        for (char c : t.toCharArray()) {
-            if(hm.get(c)==null||hm.get(c)==0)return false;
-            hm.put(c, hm.get(c)-1);
-        }
-        return true;
     }
 }
