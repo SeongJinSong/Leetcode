1acc32b65b4a0007589e583c0cac16d5d3ce7dbf
송성진 <seongjin_song@tmax.co.kr>
2022-08-02 20:36:32 +0900

    장난꾸러기

---
+++ src/main/java/com/study/inflearn/sorting_and_searching/Mischief.java
@@ -0,0 +1,25 @@
+package com.study.inflearn.sorting_and_searching;
+
+import com.study.inflearn.P;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mischief {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] b = a.clone();
+        Arrays.sort(b);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]!=b[i])res.add(i+1);
+        }
+        P.print(res);
+    }
+}
