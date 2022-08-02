e56e73c74596fe7f4903a6363daea5a39261f5c0
seongjin_song <seongjin_song@tmax.co.kr>
2022-08-02 13:08:26 +0900

    쇠막대기 (stack)

---
+++ src/main/java/com/study/inflearn/stack/IronStick.java
@@ -0,0 +1,47 @@
+package com.study.inflearn.stack;
+
+import java.util.*;
+
+public class IronStick {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String converted = s.replaceAll("\\(\\)", "-");
+        ArrayList<Integer> a = new ArrayList<>();
+        int ans = 0;
+        for (char c : converted.toCharArray()) {
+            if(c=='(') {
+                a.add(0);
+            } else if (c == '-') {
+                for (int i = 0; i < a.size(); i++) {
+                    a.set(i, a.get(i) + 1);
+                }
+            } else {
+                ans += a.remove(a.size() - 1)+1;
+            }
+        }
+        System.out.println(ans);
+        System.out.println("---------------");
+        IronStick T = new IronStick();
+        System.out.println(T.solution(s));
+    }
+
+    private int solution(String s) {
+        Stack<Character> stk = new Stack<>();
+        int ans = 0;
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i) == '(' && i < s.length() - 1 && s.charAt(i + 1) == ')') {
+                ans += stk.size();
+                i++;
+            }
+            else if (s.charAt(i) == '(' && i < s.length() - 1 && s.charAt(i + 1) != ')') {
+                stk.push(s.charAt(i));
+            }
+            else {
+                stk.pop();
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
