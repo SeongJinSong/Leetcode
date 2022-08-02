e6234425f1896145b326dbfb1a47cbbc8cf35947
송성진 <seongjin_song@tmax.co.kr>
2022-08-02 19:51:18 +0900

    버블 정렬

---
+++ src/main/java/com/study/inflearn/sorting_and_searching/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.study.inflearn.sorting_and_searching;
+
+import com.study.inflearn.P;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        P.print(a);
+    }
+}
