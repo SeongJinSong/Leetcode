4671d60f1fed13f9a12d966993aef9f373589eae
송성진 <seongjin_song@tmax.co.kr>
2022-08-02 19:12:56 +0900

    선택 정렬

---
+++ src/main/java/com/study/inflearn/sorting_and_searching/SelectSort.java
@@ -0,0 +1,24 @@
+package com.study.inflearn.sorting_and_searching;
+
+import com.study.inflearn.P;
+
+import java.util.Scanner;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)a[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if(a[idx]>a[j])idx=j;
+            }
+            int temp = a[idx];
+            a[idx] = a[i];
+            a[i] = temp;
+        }
+        P.print(a);
+    }
+}
