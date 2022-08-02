453e5fc749736cfc4ab2a7a435f93379707b196d
송성진 <seongjin_song@tmax.co.kr>
2022-08-02 20:01:58 +0900

    삽입 정렬

---
+++ src/main/java/com/study/inflearn/sorting_and_searching/InsertSort.java
@@ -0,0 +1,23 @@
+package com.study.inflearn.sorting_and_searching;
+
+import com.study.inflearn.P;
+
+import java.util.Scanner;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int tmp=a[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if(a[j]>tmp)a[j+1]=a[j];
+                else break;
+            }
+            a[j+1]=tmp;
+        }
+        P.print(a);
+    }
+}
