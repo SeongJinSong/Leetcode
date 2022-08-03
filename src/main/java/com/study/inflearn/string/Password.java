86e91c03229be042d9b2d59273c98a1d0487405e
seongjin_song <seongjin_song@tmax.co.kr>
2022-08-03 23:15:25 +0900

    암호 (Integer.parseInt(num, 2) 를 통한 10진수 변환)

--- src/main/java/com/study/inflearn/string/Password.java
+++ src/main/java/com/study/inflearn/string/Password.java
@@ -12,19 +12,8 @@ public class Password {
         s = s.replaceAll("#", "1").replaceAll("\\*", "0");
         char[] a = new char[n];
         for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(s.substring(i * 7, (i+1) * 7 ));
-            a[i] = (char)convertDecimal(tmp);
+            a[i] = (char)Integer.parseInt(s.substring(i * 7, (i+1) * 7 ), 2);
         }
         System.out.println(String.valueOf(a));
-    }
-
-    private static int convertDecimal(int n) {
-        int ans = 0;
-        int cnt = 0;
-        while (n > 0) {
-            ans += (n % 10) * Math.pow(2, cnt++);
-            n/=10;
-        }
-        return ans;
     }
 }
