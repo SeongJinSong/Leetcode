1e9341e0085b54399e334c072e9eab84b4684e58
seongjin_song <seongjin_song@tmax.co.kr>
2022-08-03 22:50:43 +0900

    문자열 압축 (마지막 변경사항 없는경우 예외처리)

--- src/main/java/com/study/inflearn/string/StringCompression.java
+++ src/main/java/com/study/inflearn/string/StringCompression.java
@@ -9,11 +9,16 @@ public class StringCompression {
         int cnt = 0;
         char prev = 0;
         StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
+        char[] ch = new char[s.length() + 1];
+        for (int i = 0; i < ch.length; i++) {
+            if(i==ch.length-1)ch[i]=0;
+            else ch[i] = s.charAt(i);
+        }
+        for (char c : ch) {
             if(prev==c)cnt++;
             else{
                 if(cnt>1)sb.append(cnt);
-                sb.append(c);
+                if(c!=0)sb.append(c);
                 prev=c;
                 cnt=1;
             }
